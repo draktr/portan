2d7d96ea7c44c4e55d645ea96286dd86341a228d
Dragan <dragan@u.yale-nus.edu.sg>
2022-07-17 23:23:01 +0200

    add cumulative returns and basic plots

--- portfolio_analytics.py
+++ portfolio_analytics.py
@@ -5,12 +5,12 @@ import yfinance as yf
 
 
 class PortfolioAnalytics():
-    def _init_(self, tickers, period):
+    def __init__(self, tickers, period):
         self.tickers=tickers
         self.period=period
 
     def list_securities(self):
-        for ticker in self.tickers
+        for ticker in self.tickers:
             security = yf.Ticker(ticker)
             print(security.info["longName"])
 
@@ -20,12 +20,12 @@ class PortfolioAnalytics():
             prices = yf.download(ticker, self.period)
             daily_returns = prices["Adj Close"].pct_change()
             all_securities_returns[ticker] = daily_returns
+        all_securities_returns.to_numpy()
         return all_securities_returns
 
     def portfolio_returns(self, weights):
         returns = self.all_securities_returns
-        returns_array = returns.to_numpy()
-        portfolio_returns = np.dot(returns_array, weights)
+        portfolio_returns = np.dot(returns, weights)
         return portfolio_returns
 
     def portfolio_daily_volatility(self, weights):
@@ -33,8 +33,41 @@ class PortfolioAnalytics():
         std = np.std(portfolio_returns)
         return std
 
-    def plot_
-# TODO: plot of daily, monthly returns
-# TODO: test
+    def portfolio_cumulative_returns(self, weights):
+        portfolio_returns = self.portfolio_returns(weights)
+        portfolio_cumulative_returns = (portfolio_returns + 1).cumprod()
+        return portfolio_cumulative_returns
+
+    def plot_portfolio_returns(self, weights):
+        fig=mpl.figure()
+        ax1=fig.add_axes([0.1,0.1,0.8,0.8])
+        ax1.plot(self.portfolio_returns(weights))
+        ax1.set_xlabel("Date")
+        ax1.set_ylabel("Daily Returns")
+        ax1.set_title("Portfolio Daily Returns")
+        mpl.show()
+
+    def plot_portfolio_returns_distribution(self, weights):
+        portfolio_returns = self.portfolio_returns(weights)
+
+        fig = mpl.figure()
+        ax1 = fig.add_axes([0.1,0.1,0.8,0.8])
+        portfolio_returns.plot.hist(bins = 90)
+        ax1.set_xlabel("Daily returns")
+        ax1.set_ylabel("Frequency")
+        ax1.set_title("Portfolio Returns Distribution")
+        mpl.show()
+
+    def plot_portfolio_cumulative_returns(self, weights):
+        portfolio_cumulative_returns = self.portfolio_cumulative_returns(weights)
+
+        fig = mpl.figure()
+        ax1 = fig.add_axes([0.1,0.1,0.8,0.8])
+        portfolio_cumulative_returns.plot()
+        ax1.set_xlabel("Date")
+        ax1.set_ylabel("Cumulative Returns")
+        ax1.set_title("Portfolio Cumulative Returns")
+        mpl.show()
 
 
+# TODO: same analytics for each security in the portfolio separately
