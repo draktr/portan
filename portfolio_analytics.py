6eedbb037418461b54995f17ebbd16b791ccda8f
Dragan <dragan@u.yale-nus.edu.sg>
2022-07-22 23:37:47 +0200

    add ulcer, mdd, plot_ulcer, martin methods and portfolio_state property

--- portfolio_analytics.py
+++ portfolio_analytics.py
@@ -5,11 +5,12 @@ import yfinance as yf
 
 
 class PortfolioAnalytics():
-    def __init__(self, tickers, weights, start, end, risk_free_rate=0.01):
+    def __init__(self, tickers, weights, start, end, initial_aum=10000, risk_free_rate=0.01):
         self.tickers=tickers
         self.start=start
         self.end=end
         # ? self.weights = weights
+        self.initial_aum = initial_aum
         self.risk_free_rate = risk_free_rate
 
         self.securities_returns = pd.DataFrame(columns=self.tickers)
@@ -17,10 +18,19 @@ class PortfolioAnalytics():
         for ticker in self.tickers:
             price_current = yf.download(ticker, start=self.start, end=self.end)
             self.prices[ticker] = price_current["Adj Close"]
-            returns_current = self.prices["Adj Close"].pct_change()
-            self.securities_returns[ticker] = returns_current
+            self.securities_returns[ticker] = price_current["Adj Close"].pct_change()
 
-        #returns = self.securities_returns.to_numpy()
+        # funds allocated to each security
+        self.funds_allocation = np.multiply(self.initial_aum, weights)
+
+        # number of securities bought at t0
+        self.initial_number_of_securities_bought = np.divide(self.funds_allocation, self.prices.iloc[0])
+
+        # absolute (dollar) value of each security in portfolio (i.e. state of the portfolio, not rebalanced)
+        self.portfolio_state = np.multiply(self.prices, self.initial_number_of_securities_bought)
+        self.portfolio_state["Whole Portfolio"] = self.portfolio_state.sum(axis=1)
+
+        # portfolio returns (numpy array)
         self.portfolio_returns = np.dot(self.securities_returns.to_numpy(), weights)
         #TODO: if-else about importing .csv data, periods etc
 
@@ -96,14 +106,64 @@ class PortfolioAnalytics():
 
     def sortino(self):
         mean_daily_returns = np.mean(self.portfolio_returns)
-        downside_volatility = self.downside_deviation(self.risk_free_rate)
+        downside_volatility = self.downside_deviation()
         sortino_ratio = (100*mean_daily_returns - self.risk_free_rate)/downside_volatility
         return sortino_ratio
 
+    def maximum_drawdown(self, period=365):
+        peak = np.max(self.portfolio_state.iloc[-period:]["Whole Portfolio"])
+        peak_index = self.portfolio_state["Whole Portfolio"].idxmax()
+        peak_index_int = self.portfolio_state.index.get_loc(peak_index)
+        trough = np.min(self.portfolio_state.iloc[-peak_index_int:]["Whole Portfolio"])
+
+        maximum_drawdown = (trough-peak)/peak
+        return maximum_drawdown
+
+    def ulcer(self, period=14, start=1):
+        close = np.empty(period)
+        percentage_drawdown = np.empty(period)
+
+        if start==1:
+            period_high = np.max(self.portfolio_state.iloc[-period:]["Whole Portfolio"])
+        else:
+            period_high = np.max(self.portfolio_state.iloc[-period-start+1:-start+1]["Whole Portfolio"])
+
+        for i in range(period):
+            close[i] = self.portfolio_state.iloc[-i-start+1]["Whole Portfolio"]
+            percentage_drawdown[i] = 100*((close[i] - period_high))/period_high
+
+        ulcer_index = np.sqrt(np.mean(np.square(percentage_drawdown)))
+        return ulcer_index
+
+    def plot_ulcer(self, period=14):
+        ulcer_values = pd.DataFrame(columns=["Ulcer Index"], index=self.portfolio_state.index)
+        for i in range(len(self.portfolio_state)-period):
+            ulcer_values.iloc[-i]["Ulcer Index"] = self.ulcer(period, start=i)
+
+        fig=mpl.figure()
+        ax1=fig.add_axes([0.1,0.1,0.8,0.8])
+        ulcer_values["Ulcer Index"].plot()
+        ax1.set_xlabel("Date")
+        ax1.set_ylabel("Ulcer Index")
+        ax1.set_title("Portfolio Ulcer Index")
+        mpl.show()
+
+    def martin(self, period=14):
+        mean_daily_returns = np.mean(self.portfolio_returns)
+        ulcer_index = self.ulcer(period)
+        sharpe_ratio = (100*mean_daily_returns - self.risk_free_rate)/ulcer_index
+        return sharpe_ratio
 
 # TODO: same analytics for each security in the portfolio separately
-# TODO: other ratios, maximum drawdown; martin ratio
+# TODO: other ratios
 # TODO: ulcer index adn other measurements of pain
 # TODO: do portfolio metrics in one method
 # TODO: separate one for checking which weigths
 # TODO: asset covariance or correlation matrix
+# TODO figure out 100* percentage stuff for the ratios
+# TODO: look into martin, sharepe etc bc nans
+
+# TODO: figure out what else can be put in the __init__() (mean_daily_return etc)
+# TODO: market report (indices, yield curve etc)
+
+# cloud providers, chip names (SOXX), cyber security
