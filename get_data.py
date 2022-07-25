8c312b50dcda2cd84611d0c629ad4b52bf47ba19
Dragan <dragan@u.yale-nus.edu.sg>
2022-07-26 01:20:09 +0200

    add methods for getting long, wide and adj close only data

--- get_data.py
+++ get_data.py
@@ -12,21 +12,28 @@ class GetData():
 
         if self.period==None:
             for ticker in self.ticker:
-                time_series = yf.download(ticker, group_by="Ticker", begin=self.begin, end=self.end)
+                time_series = yf.download(ticker, begin=self.begin, end=self.end)
                 time_series["ticker"] = ticker
                 self.df_list.append(time_series)
-
         else:
             for ticker in self.ticker:
-                time_series = yf.download(ticker, group_by="Ticker", period=self.period)
+                time_series = yf.download(ticker, period=self.period)
                 time_series["ticker"] = ticker
                 self.df_list.append(time_series)
 
-        # combine all dataframes into a single dataframe
-        self.data = pd.concat(self.df_list)
+    def save_all_long(self):
+        data_long = pd.concat(self.df_list, axis=0)
+        data_long.to_csv("all_tickers_data_long.csv")
 
-    def save_together(self):
-        self.data.to_csv("all_tickers_data.csv")
+    def save_all_wide(self):
+        data_wide = pd.concat(self.df_list, axis=1)
+        data_wide.to_csv("all_tickers_data_wide.csv")
+
+    def save_adj_close_only(self):
+        adj_close_only = pd.DataFrame(columns=self.ticker)
+        for ticker, i in zip(self.ticker, range(len(self.df_list))):
+            adj_close_only[ticker] = self.df_list[i]["Adj Close"]
+        adj_close_only.to_csv("adj_close_only.csv")
 
     def save_separately(self):
         os.mkdir("tickers_data")
