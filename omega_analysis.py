2192affdde114173374dd18a2138533422bf685d
Dragan <dragan@u.yale-nus.edu.sg>
2022-07-27 00:58:12 +0200

    initial commit

---
+++ omega_analysis.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+
+class OmegaAnalysis():
+    def __init__(self) -> None:
+        pass
+
+    def omega_ratio(self):
+        pass
+
+    def omega_curve(self):
+        pass
