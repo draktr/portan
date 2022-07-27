369b56c4b0391f599a37301e02982e551b6883d0
Dragan <dragan@u.yale-nus.edu.sg>
2022-07-27 15:06:27 +0200

    parametrize threshold bounds, add save and show options for omega_curve(), add docstrings

--- omega_analysis.py
+++ omega_analysis.py
@@ -3,12 +3,31 @@ import pandas as pd
 import matplotlib.pyplot as mpl
 
 class OmegaAnalysis():
-    def __init__(self, returns):
-        self.returns=returns # Dataframe with the daily returns of different portfolios
-                             # Each col is a return time series of a particular portfolio to be compared
-        self.thresholds = np.linspace(0, 1, 100)
+    def __init__(self, returns, threshold_lower=0, threshold_upper=0.2):
+        """
+        Initiates the object.
+
+        Args:
+            returns (pd.DataFrame): Dataframe with the daily returns of different portfolios.
+            threshold_lower (int, optional): Threshold lower bound for the Omega Curve. Defaults to 0.
+            threshold_upper (float, optional): Threshold upper bound for the Omega Curve. Defaults to 0.2.
+        """
+
+        self.returns=returns
+        self.thresholds=np.linspace(threshold_lower, threshold_upper, round(100*(threshold_upper-threshold_lower)))
 
     def omega_ratio(self, portfolio_returns=None, annual_threshold=0.03):
+        """
+        Calculates the Omega Ratio of one or more portfolios.
+
+        Args:
+            portfolio_returns (pd.DataFrame, optional): Dataframe with the daily returns of single or more portfolios. Defaults to None.
+            annual_threshold (float, optional): Minimum Acceptable Return (decimal). Defaults to 0.03.
+
+        Returns:
+            pd.Series: Series with Omega Ratios of all portfolios.
+        """
+
         daily_threshold = (annual_threshold + 1)**np.sqrt(1/252)-1
 
         if portfolio_returns is None:
@@ -25,8 +44,17 @@ class OmegaAnalysis():
             omega=winning/losing
         return omega
 
-    def omega_curve(self):
+    def omega_curve(self, show=True, save=False):
+        """
+        Plots and/or saves Omega Curve(s) of of one or more portfolios.
+
+        Args:
+            show (bool, optional): Show the plot upon the execution of the code. Defaults to True.
+            save (bool, optional): Save the plot on storage. Defaults to False.
+        """
+
         all_values = pd.DataFrame(columns=self.returns.columns)
+
         for portfolio in self.returns.columns:
             omega_values = list()
             for threshold in self.thresholds:
@@ -35,4 +63,7 @@ class OmegaAnalysis():
             all_values[portfolio] = omega_values
 
         all_values.plot(title="Omega Curve", xlabel="Minimum Acceptable Return (%)", ylabel="Omega Ratio", ylim=(0, 1.5))
-        mpl.show()
+        if show==True:
+            mpl.show()
+        if save==True:
+            mpl.savefig("omega_curves.png")
