afbb4cb3909f1a7440b94b504538892e681f30c7
Dragan <dragan@u.yale-nus.edu.sg>
2022-07-29 15:21:53 +0200

    add capm analytics, save self.start/end when importing data from .csv

--- portfolio_analytics.py
+++ portfolio_analytics.py
@@ -1,21 +1,24 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as mpl
+from sklearn.linear_model import LinearRegression
+from datetime import datetime
 import yfinance as yf
 
 
 class PortfolioAnalytics():
-    def __init__(self, tickers, weights, start, end, data = None, initial_aum=10000, risk_free_rate=0.03):
+    def __init__(self, tickers, weights, start="1970-01-01", end=str(datetime.now())[0:10], data=None, initial_aum=10000, risk_free_rate=0.03):
         self.tickers=tickers
         self.start=start
         self.end=end
-        self.data=data # takes in only the data of type GetData.save_adj_close_only()
+        self.data=data # takes in only the data of kind GetData.save_adj_close_only()
         self.initial_aum = initial_aum
         self.risk_free_rate = risk_free_rate # takes in decimals (not percentage)
 
-        if data==None:
-            self.securities_returns = pd.DataFrame(columns=self.tickers)
+        if data is None:
             self.prices = pd.DataFrame(columns=self.tickers)
+            self.securities_returns = pd.DataFrame(columns=self.tickers)
+
             for ticker in self.tickers:
                 price_current = yf.download(ticker, start=self.start, end=self.end)
                 self.prices[ticker] = price_current["Adj Close"]
@@ -24,6 +27,8 @@ class PortfolioAnalytics():
             self.prices = pd.read_csv(self.data, index_col=["Date"])
             self.securities_returns = pd.DataFrame(columns=self.tickers, index=self.prices.index)
             self.securities_returns = self.prices.pct_change()
+            self.start = self.prices.index[0]
+            self.end = self.prices.index[-1]
 
         # funds allocated to each security
         self.funds_allocation = np.multiply(self.initial_aum, weights)
@@ -90,10 +95,62 @@ class PortfolioAnalytics():
         ax1.set_title("Portfolio Cumulative Returns")
         mpl.show()
 
-    def average_daily_return(self):
-        returns = self.returns_with_dates()
-        average_return = returns.mean()
-        return average_return
+    def excess_return(self, minimum_acceptable_return=0.03):
+        excess_return = self.portfolio_returns - minimum_acceptable_return
+        return excess_return
+
+    def benchmark(self, benchmark_tickers, benchmark_weights, data=None):
+        if data is None:
+            benchmark_securities_prices = pd.DataFrame(columns=benchmark_tickers)
+            benchmark_securities_returns = pd.DataFrame(columns=benchmark_tickers)
+
+            for ticker in benchmark_tickers:
+                price_current = yf.download(ticker, start=self.start, end=self.end)
+                benchmark_securities_prices[ticker] = price_current["Adj Close"]
+                benchmark_securities_returns[ticker] = price_current["Adj Close"].pct_change()
+
+            benchmark_returns = np.dot(benchmark_securities_returns.to_numpy(), benchmark_weights)
+        else:
+            benchmark_securities_prices = pd.read_csv(data, index_col=["Date"]) # takes in only the data of kind GetData.save_adj_close_only()
+            benchmark_securities_returns = pd.DataFrame(columns=benchmark_tickers, index=benchmark_securities_prices.index)
+            benchmark_securities_returns = benchmark_securities_prices.pct_change()
+
+            benchmark_returns = np.dot(benchmark_securities_returns.to_numpy(), benchmark_weights)
+
+        return benchmark_returns
+
+    def capm(self, benchmark_tickers, benchmark_weights, data=None):
+        excess_portfolio_returns = self.portfolio_returns - self.risk_free_rate
+        excess_benchmark_returns = self.benchmark(benchmark_tickers, benchmark_weights, data)
+        excess_benchmark_returns = excess_benchmark_returns - self.risk_free_rate
+
+        model = LinearRegression().fit(excess_benchmark_returns, excess_portfolio_returns)
+        alpha = model.intercept_
+        beta = model.coef_
+        r_squared = model.score(excess_benchmark_returns, excess_portfolio_returns)
+
+        return alpha, beta, r_squared
+
+    def plot_capm(self, benchmark_tickers, benchmark_weights, data=None, show=True, save=False):
+
+        excess_portfolio_returns = self.portfolio_returns - self.risk_free_rate
+        excess_benchmark_returns = self.benchmark(benchmark_tickers, benchmark_weights, data)
+        excess_benchmark_returns = excess_benchmark_returns - self.risk_free_rate
+
+        beta, alpha = np.polyfit(excess_benchmark_returns, excess_portfolio_returns, 1)
+
+        fig=mpl.figure()
+        ax1=fig.add_axes([0.1,0.1,0.8,0.8])
+        ax1.scatter(excess_benchmark_returns, excess_portfolio_returns)
+        ax1.plot(excess_benchmark_returns, alpha+beta*excess_benchmark_returns)
+        ax1.legend(labels=[r'$\alpha$', r'$\beta$']) #TODO check if this works
+        ax1.set_xlabel("Benchmark Excess Returns")
+        ax1.set_ylabel("Portfolio Excess Returns")
+        ax1.set_title("Portfolio Excess Returns Against Benchmark")
+        if save is True:
+            mpl.savefig("capm.png", dpi=300)
+        if show is True:
+            mpl.show()
 
     def sharpe(self):
         sharpe_ratio = 100*(self.mean_daily_returns - self.risk_free_rate)/self.volatility
@@ -105,17 +162,22 @@ class PortfolioAnalytics():
         upside_volatility = np.std(positive_portfolio_returns)
         return upside_volatility
 
+    def downside_volatility(self, returns=None): #? use MAR instead of RFR?
+        if returns is None:
+            negative_portfolio_returns = self.portfolio_returns - self.risk_free_rate
+        else:
+            negative_portfolio_returns = returns - self.risk_free_rate
+
+        negative_portfolio_returns = negative_portfolio_returns[negative_portfolio_returns<0]
+        downside_volatility = np.std(negative_portfolio_returns)
+        return downside_volatility
+
     def volatility_skewness(self):
         upside = self.upside_volatility()
         downside = self.downside_volatility()
         skewness = upside/downside
         return skewness
 
-    def downside_volatility(self):
-        negative_portfolio_returns = self.portfolio_returns - self.risk_free_rate
-        negative_portfolio_returns = negative_portfolio_returns[negative_portfolio_returns<0]
-        downside_volatility = np.std(negative_portfolio_returns)
-        return downside_volatility
 
     def sortino(self):
         downside_volatility = self.downside_volatility()
@@ -182,6 +244,10 @@ class PortfolioAnalytics():
 # TODO: do portfolio metrics in one method
 # TODO: separate one for checking which weigths
 # TODO: asset covariance or correlation matrix
+# TODO: excess stuff for alpha and beta analysis
+# TODO: mpl to plt
+# TODO: replace threshold with MAR, in omega_analysis as well
 # ? annualizing ratios?
+# period=len(data) | x*np.sqrt(period)
 
 # cloud providers, chip names (SOXX), cyber security
