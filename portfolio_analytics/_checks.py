49b9fec480cf446baf4872d3f65ace4950e4f8a6
Dragan <dragan@u.yale-nus.edu.sg>
2023-04-08 00:19:36 +0200

    Added checks for percentage arguments and array of returns for multiple portfolios

--- portfolio_analytics/_checks.py
+++ portfolio_analytics/_checks.py
@@ -1,11 +1,11 @@
 import numpy as np
+import pandas as pd
 import warnings
 
 
 def _check_rate_arguments(
     annual_mar=None, annual_rfr=None, annual=None, compounding=None
 ):
-
     if not isinstance(annual_mar, (float, type(None))):
         raise ValueError()
     if not isinstance(annual_rfr, (float, type(None))):
@@ -23,7 +23,6 @@ def _check_rate_arguments(
 
 
 def _check_plot_arguments(show, save):
-
     if not isinstance(show, bool):
         raise ValueError()
     if not isinstance(save, bool):
@@ -31,7 +30,6 @@ def _check_plot_arguments(show, save):
 
 
 def _check_period(period, portfolio_state):
-
     if not isinstance(period, int):
         raise ValueError()
 
@@ -47,3 +45,21 @@ def _check_posints(argument):
         raise ValueError()
     if argument < 1:
         raise ValueError()
+
+
+def _check_percentage(percentage):
+    if not isinstance(percentage, bool):
+        raise ValueError("Percentage argument should be a `bool`.")
+
+
+def _check_multiple_returns(returns):
+    if not isinstance(returns, (np.ndarray, pd.DataFrame, pd.Series)):
+        raise ValueError()
+    if isinstance(returns, np.ndarray):
+        returns = pd.DataFrame(returns)
+    if returns.shape[1] == 1:
+        warnings.Warn()
+    if np.any(np.isnan(returns)):
+        warnings.Warn()
+
+    return returns
