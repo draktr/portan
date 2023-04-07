7603a3e0e7301e88b84d3fd61fb97d092ed344e1
Dragan <dragan@u.yale-nus.edu.sg>
2023-04-07 15:05:47 +0200

    Switched from OOP to procedural

--- portfolio_analytics/utilities.py
+++ portfolio_analytics/utilities.py
@@ -1,63 +1,56 @@
-class Utils(PortfolioAnalytics):
-    def __init__(
-        self,
-        data,
-        weights,
-        portfolio_name="Investment Portfolio",
-        initial_aum=10000,
-        frequency=252,
-    ) -> None:
+import pandas as pd
 
-        super.__init__(data, weights, portfolio_name, initial_aum, frequency)
 
-    def concatenate_portfolios(self, portfolio_one, portfolio_two):
-        """
-        Concatenates an array of portfolio returns to an existing array of portfolio returns.
-        Accepts array-like objects such as np.ndarray, pd.DataFrame, pd.Series, list etc.
+def concatenate_portfolios(portfolio_one, portfolio_two):
+    """
+    Concatenates an array of portfolio returns to an existing array of portfolio returns.
+    Accepts array-like objects such as np.ndarray, pd.DataFrame, pd.Series, list etc.
 
-        Args:
-            portfolio_one (array-like object): Returns of first portfolio(s).
-            portfolio_two (array-like object): Returns of portfolio(s) to be concatenated to the right.
+    Args:
+        portfolio_one (array-like object): Returns of first portfolio(s).
+        portfolio_two (array-like object): Returns of portfolio(s) to be concatenated to the right.
 
-        Returns:
-            pd.DataFrame: DataFrame with returns of the given portfolios in respective columns.
-        """
+    Returns:
+        pd.DataFrame: DataFrame with returns of the given portfolios in respective columns.
+    """
 
-        portfolios = pd.concat(
-            [pd.DataFrame(portfolio_one), pd.DataFrame(portfolio_two)], axis=1
-        )
+    portfolios = pd.concat(
+        [pd.DataFrame(portfolio_one), pd.DataFrame(portfolio_two)], axis=1
+    )
 
-        return portfolios
+    return portfolios
 
-    def periodization(self, given_rate, periods=1 / 252):
-        """
-        Changes rate given in one periodization into another periodization.
-        E.g. annual rate of return into daily rate of return etc.
 
-        Args:
-            given_rate (float): Rate of interest, return etc. Specified in decimals.
-            periods (float, optional): How many given rate periods there is in one output rate period.
-                                       Defaults to 1/252. Converts annual rate into daily rate given 252 trading days.
-                                       periods=1/365 converts annual rate into daily (calendar) rate.
-                                       periods=252 converts daily (trading) rate into annual rate.
-                                       periods=12 converts monthly rate into annual.
+def periodization(given_rate, periods=1 / 252):
+    """
+    Changes rate given in one periodization into another periodization.
+    E.g. annual rate of return into daily rate of return etc.
 
-        Returns:
-            float: Rate expressed in a specified period.
-        """
-        # TODO: should self.frequence be involved here?
-        output_rate = (given_rate + 1) ** (periods) - 1
+    Args:
+        given_rate (float): Rate of interest, return etc. Specified in decimals.
+        periods (float, optional): How many given rate periods there is in one output rate period.
+                                    Defaults to 1/252. Converts annual rate into daily rate given 252 trading days.
+                                    periods=1/365 converts annual rate into daily (calendar) rate.
+                                    periods=252 converts daily (trading) rate into annual rate.
+                                    periods=12 converts monthly rate into annual.
 
-        return output_rate
+    Returns:
+        float: Rate expressed in a specified period.
+    """
+    # TODO: should self.frequence be involved here?
+    output_rate = (given_rate + 1) ** (periods) - 1
 
-    def fill_nan(self, portfolio_returns, method="adjacent", data_object="pandas"):
+    return output_rate
 
-        if data_object == "numpy":  # TODO: _checks.py here
-            portfolio_returns = pd.DataFrame(portfolio_returns)
 
-        if method == "adjacent":
-            portfolio_returns.interpolate(method="linear", inplace=True)
-        elif method == "column":
-            portfolio_returns.fillna(portfolio_returns.mean(), inplace=True)
-        else:
-            raise ValueError("Fill method unsupported.")
+def fill_nan(portfolio_returns, method="adjacent", data_object="pandas"):
+
+    if method == "adjacent":
+        portfolio_returns.interpolate(method="linear", inplace=True)
+    elif method == "column":
+        portfolio_returns.fillna(portfolio_returns.mean(), inplace=True)
+    else:
+        raise ValueError("Fill method unsupported.")
+
+
+# TODO: returns of multiple portfolios at once vixen 2d array of weights and tickers
