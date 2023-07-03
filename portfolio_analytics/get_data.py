8886d1524bdbbf9221051b91a1fd5179cb62c234
Dragan <dragan@u.yale-nus.edu.sg>
2023-07-03 21:34:51 +0200

    Added docstrings

--- portfolio_analytics/get_data.py
+++ portfolio_analytics/get_data.py
@@ -10,6 +10,19 @@ class GetData:
     def __init__(
         self, tickers, start="1970-01-02", end=CURRENT_DATE, interval="1d", **kwargs
     ):
+        """
+        Initialtes GetData object by downloading data from `yfinance` which the user can use within `Python` or save as `.csv`
+
+        :param tickers: Tickers of assets for which data is to be downloaded
+        :type tickers: list
+        :param start: Download start date for the data, defaults to "1970-01-02"
+        :type start: str, optional
+        :param end: Download end date for the data, defaults to CURRENT_DATE
+        :type end: str, optional
+        :param interval: Data interval. Valid intervals are: 1m, 2m, 5m, 15m, 30m, 60m, 90m, 1h, 1d, 5d, 1wk, 1mo, 3mo, defaults to "1d"
+        :type interval: str, optional
+        """
+
         if len(tickers) == 1:
             self._data = yf.Ticker(tickers[0]).history(
                 start=start, end=end, interval=interval, **kwargs
@@ -21,13 +34,31 @@ class GetData:
 
     @property
     def data(self):
+        """
+        Gives access to the full DataFrame of the downloaded data
+
+        :return: Data downloaded
+        :rtype: pd.DataFrame
+        """
+
         return self._data
 
     @property
     def close(self):
+        """
+        Gives access to the asset prices at trading close
+
+        :return: Data downloaded
+        :rtype: pd.DataFrame
+        """
+
         return self._data["Close"]
 
     def save_all_long(self):
+        """
+        Saves downloaded data as `.csv` in long format
+        """
+
         data_long = (
             self.data.stack(level=1)
             .reset_index(1)
@@ -37,12 +68,24 @@ class GetData:
         data_long.to_csv("all_tickers_data_long.csv")
 
     def save_all_wide(self):
+        """
+        Saves downloaded data as `.csv` in wide format
+        """
+
         self.data.to_csv("all_tickers_data_wide.csv")
 
     def save_close_only(self):
+        """
+        Saves trading close prices as `.csv`
+        """
+
         self.close.to_csv("close_only.csv")
 
     def save_separately(self):
+        """
+        Saves trading data as `.csv` for each asset separately
+        """
+
         os.mkdir("tickers_data")
         os.chdir("tickers_data")
         current_data = self.data
